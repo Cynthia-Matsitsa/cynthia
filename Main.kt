0712f1d144b2560b209f8dd40093e41b2d23f642
Cynthia Matsitsa <cynthiamatsitsa@gmail.com>
2024-02-07 08:32:35 +0300

    add file

---
+++ Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var fullName: String = ("cynthia")
+    println("Full Name : Cynthia")
+    var age: Byte = 22
+    println("Age : 22")
+    var phoneNumber: String = ("+254791859787")
+    println("Phone Number : +254791859787")
+    var weightInKg: Double = 71.5
+    println("Weight In Kg :71.5")
+    var kenyanCitizen: Boolean = true
+    println("Kenyan Citizen")
+}
